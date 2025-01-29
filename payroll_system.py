2e211bbfab7e1a0ff6a59c68006826831a2bf231
Geraldine Cassie <cassiemakaza447@gmail.com>
2025-01-29 04:37:23 +0200

    Completed all the functions

--- payroll_system.py
+++ payroll_system.py
@@ -7,8 +7,10 @@ list_of_employee_numbers = ['00635','87958','09783']
 
 #Print function
 def greetings():
+    print("=========================================")
     print("WELCOME TO KASSIDY PAYROLL SYSTEM")
     print("MENU:")
+    print("=========================================")
 
 
 #View employees
@@ -16,9 +18,6 @@ def view():
     for index,surname in enumerate(list_of_surnames):
         print(f" {index + 1}) {list_of_emp_names[index]} {surname} {list_of_employee_numbers[index]}")
 
-
-    # for i in range(len(list_of_emp_names)):
-    #     print(list_of_emp_names[i]+ " "+list_of_surnames[i]+ " "+(str(list_of_employee_numbers[i])))
 
 def add_new_employee():
     """
@@ -36,56 +35,54 @@ def add_new_employee():
 
 def delete_employee():
 
-    # print("List of names:")
-    # #Enumerate shows the index number and the name.
-    # for i, name in enumerate(list_of_emp_names):
-    #     print(f"{i + 1}:{name}")
+    print("=========================================")
+    print("List of names:")
+    #Enumerate shows the index number and the name.
+    for i, name in enumerate(list_of_emp_names):
+        print(f"{i + 1}:{name}")
 
-    # delete_name = input("Delete name?")
-    # #Coverted the input into an interger because the input function only takes strings.
-    # new_del_name = int(delete_name)
-    # del list_of_emp_names[new_del_name-1]
+    delete_name = input("Delete name?")
+    #Coverted the input into an interger because the input function only takes strings.
+    new_del_name = int(delete_name)
+    del list_of_emp_names[new_del_name-1] #-1 so that the index number can start from 0, cause
+    #we incremented when we were printing the list, so that the user does not see the first
+    # name printed on option 0, however when we now want to delete the first index we have to
+    #decrement the index by 1 so that it matches.
 
-    # print(list_of_employee_numbers)
-
-    # for name in list_of_emp_names:
-    #     print(name)
+    print("Remaining employee names")
+    for name in list_of_emp_names:
+        print(name)
 
     """_deleting by surname_
     """
-    print("=========================================")
     print("List of employee surnames")
     #prints the lists of surnames and their index numbers.
     for index,surname in enumerate(list_of_surnames):
-        print(index + 1,surname)
+        print(f"{index + 1}:{surname}")
 
-    #remember the input function only takes in strings,sss
-    #so if i want the user to choose a number I have to convert
-    #whatever they
     delete_surname = input("Delete surname:")
     new_del_surname = int(delete_surname)
+    del list_of_surnames[new_del_surname-1]
 
+    print("Remaining employee surnames")
+    for surname in list_of_surnames:
+        print(surname)
 
+    """Deleting by employee numbers.
+    """
+    #Displaying employee number and incremented the index number so that the user can see it starting from 1 instead of 0.
+    print("Employee Numbers:\n")
+    for num_index,number in enumerate(list_of_employee_numbers):
+        print(f"{num_index + 1}:{number}")
 
+    delete_emp_number=input("Select employee number to delete.")
+    del list_of_employee_numbers[int(delete_emp_number)-1]
+
+    print("Remaining employee numbers:")
+    for num in list_of_employee_numbers:
+        print(num)
 
     print("=========================================")
-
-
-    #first have to convert
-
-def update_employee():
-    update_emp_name= input("Enter employee name you want to update")
-    list_of_emp_names.update()
-
-    update_emp_surname = input("Enter employee surname you want to update")
-    list_of_surnames.update()
-
-    update_emp_number= input("Enter employee number you want to update")
-    list_of_employee_numbers.update()
-
-    print(list_of_emp_names)
-    print(list_of_surnames)
-    print(list_of_employee_numbers)
 
 
 def exit_program():
@@ -107,7 +104,7 @@ def menu():
         print("4) Update an employee")
         print("5) Quit")
 
-        option=input("What would you like to do?")
+        option=input("What would you like to do?\n")
 
         if option == "1":
         #Calling the View function
@@ -120,10 +117,10 @@ def menu():
             delete_employee()
         elif option == "4":
             #Calling the update function
-            update_employee()
-        elif option == "5":
+            add_new_employee()
+        elif option == "5\n":
             #Calling the function to quit the program.
-                exit_program()
+            exit_program()
 
 
 
